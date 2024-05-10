68477402f7ecc7804172cf2b4166e365e3709d0d
LucioZampella <162721696+LucioZampella@users.noreply.github.com>
2024-05-10 11:46:45 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,60 @@
-# Replace the "ANSWER HERE" with your answer
 
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_remove_elements) >= 6:
+        del list_to_remove_elements[0]
+        del list_to_remove_elements[3] 
+        del list_to_remove_elements[3]  
+    elif len(list_to_remove_elements) >=5:
+        del list_to_remove_elements[0]
+        del list_to_remove_elements[3]
+    else:
+        del list_to_remove_elements[0]
+
+    return list_to_remove_elements
+
+print(remove_elements(['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']))
+
+
+
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    ultimo_numero = len(list_to_add_elements)+1
+    list_to_add_elements.insert(0, "Pink")
+    list_to_add_elements.insert(ultimo_numero, "Yellow")
+
+    return list_to_add_elements
+
+print(add_elements(['Red', 'Green', 'White', 'Black']))
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_check) == 0:
+        return "La lista esta vacia"
+    else:
+        return "La lista no esta vacia"
+
+print(is_empty(""))
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if len(list_to_compare1) >= 3 and len(list_to_compare2) >= 3:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+print(check_lists(['Black', 'Pink', 'Yellow', 'Red', 'Green', 'White'], ['Red', 'Green']))
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    a = list_of_lists_to_modify[0][0:2]
+    b = list_of_lists_to_modify[1][1:4]
+    c = list_of_lists_to_modify[2][-2:]
+    list_of_lists_to_modify = [a, b, c]
+    return list_of_lists_to_modify
+    
+print(list_of_lists([[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]))
+    
